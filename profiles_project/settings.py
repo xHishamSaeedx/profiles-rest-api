aceb5d27377bba12a0bcc374dc7f7530cedb577a
Hisham <hishamsaeed2003@gmail.com>
2023-04-07 02:10:40 +0530

    Created django project and app

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles_api',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
