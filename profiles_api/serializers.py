e69a403809894210fd09458f15e4194779a77f98
Hisham <hishamsaeed2003@gmail.com>
2023-04-08 03:12:30 +0530

    added POST,PUT,PATCH and DELETE methods to APIVIEW

---
+++ profiles_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """serializes a name field for testing our APIVIEW"""
+
+    name = serializers.CharField(max_length = 10)
